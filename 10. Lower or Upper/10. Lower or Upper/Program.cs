14373706a54e6c652f5d78c80047f17b151ba485
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-03 20:21:32 +0300

    Exercises

---
+++ 10. Lower or Upper/10. Lower or Upper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int charValue = Console.Read();
+
+        if (charValue != -1)
+        {
+            char inputChar = (char)charValue;
+
+            if (char.IsUpper(inputChar))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if (char.IsLower(inputChar))
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
