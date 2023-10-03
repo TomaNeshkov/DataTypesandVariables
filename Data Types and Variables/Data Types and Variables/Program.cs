8a249c8b4735ed79b8e6030e3ae66822a70c070b
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-03 19:46:16 +0300

    Update Program.cs

--- Data Types and Variables/Data Types and Variables/Program.cs
+++ Data Types and Variables/Data Types and Variables/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        decimal m = decimal.Parse(Console.ReadLine());
+        decimal km = m / 1000;
+        Console.WriteLine($"{km:f2}");
+    }
+}
