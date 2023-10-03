0cfae28ce054172a8812ca5d257983cf54bdf8f6
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-03 19:55:06 +0300

    Exercises

---
+++ 3. Exact Sum of Real Numbers/3. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        decimal input = decimal.Parse(Console.ReadLine());
+        decimal sum = 0;
+        for (int i = 0; i < input; i++)
+        {
+            decimal num = decimal.Parse(Console.ReadLine());
+            sum += num;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
