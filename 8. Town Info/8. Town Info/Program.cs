3a2e0fce1b4e4556a6b9a19a375f05470a96a849
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-03 20:05:48 +0300

    Exercises

---
+++ 8. Town Info/8. Town Info/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    class Program
+{
+        static void Main(string[] args)
+    {
+            string name = Console.ReadLine();
+            int pop = int.Parse(Console.ReadLine());
+            int square = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {name} has population of {pop} and area {square} square km.");
+
+    }
+}
