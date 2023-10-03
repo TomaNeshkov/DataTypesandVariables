3aae3d7804f92abbc31c51d49eda857a392ea883
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-03 20:15:36 +0300

    Exercises

---
+++ 9. Chars to String/9. Chars to String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string output = string.Empty;
+        for (int i = 0; i < 3; i++)
+        {
+            char input = char.Parse(Console.ReadLine());
+            output += input;
+        }
+
+        Console.WriteLine(output);
+    }
+}
