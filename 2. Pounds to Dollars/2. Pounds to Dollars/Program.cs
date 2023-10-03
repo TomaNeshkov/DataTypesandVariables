29d755481e2b624592aca0719729353d1beaf645
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-03 19:53:27 +0300

    Exercises

---
+++ 2. Pounds to Dollars/2. Pounds to Dollars/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        decimal pounds = decimal.Parse(Console.ReadLine());
+        decimal km = pounds * 1.31m;
+        Console.WriteLine($"{km:f3}");
+    }
+}
